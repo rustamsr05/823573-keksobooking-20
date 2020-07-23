0db7b69b328bea94bfe8ed493496960ef5d9fe61
rustamsr05 <rustamsr05@gmail.com>
2020-07-23 19:39:14 +0600

    задание 8

--- js/main.js
+++ js/main.js
@@ -1,23 +1,16 @@
 'use strict';
 
 var mapFilter = document.querySelector('.map');
-
-// var avatarValues = ['img/avatars/user01.png', 'img/avatars/user02.png', 'img/avatars/user03.png', 'img/avatars/user04.png', 'img/avatars/user05.png', 'img/avatars/user06.png', 'img/avatars/user07.png', 'img/avatars/user08.png'];
-
 var titleValues = ['Уютная квартира', 'Хорошая квартира недорого', 'Квартира в центре', 'Отличный вид из окна', 'Квартира в новом доме', 'Элитное жильё', 'Квартира после капитального ремонта', 'Отличная квартира для ценителей комфорта'];
-// var adressValues = ['600, 350', '400, 350', '200, 350', '800, 350', '400, 700', '200, 7000', '100, 700', '200, 900'];
-// var priceValues = [1200, 3000, 800, 2700, 8000, 1500, 2000, 1800];
 var maxPrice = 1000000;
 var typeValues = ['flat', 'palace', 'house', 'bungalo'];
 var roomsValues = [1, 2, 3, 100];
-var guestsValues = [1, 2, 3];
+var guestsValues = ['для 1 гостя', 'для 2 гостей', 'для 3 гостей', 'не для гостей'];
 var checkinValues = ['12:00', '13:00', '14:00'];
 var checkoutValues = ['12:00', '13:00', '14:00'];
 var featuresValues = ['wifi', 'washer', 'elevator', 'conditioner', 'dishwasher', 'parking'];
 var descriptionValues = ['срочно', 'семейной паре', 'не курящим', 'на длительный срок', 'предоплата за 3 месяца', 'срочно', 'c документами', 'оплата наличными'];
 var photosValues = ['http://o0.github.io/assets/images/tokyo/hotel1.jpg', 'http://o0.github.io/assets/images/tokyo/hotel2.jpg', 'http://o0.github.io/assets/images/tokyo/hotel3.jpg'];
-// var locationХValues = [150, 450, 300, 270, 620, 200, 590, 440];
-// var locationYValues = [210, 170, 230, 520, 220, 460, 270, 540];
 var locationYMin = 130;
 var locationYMax = 630;
 
@@ -67,8 +60,8 @@ var renderBtn = function (ads) {
 mapFilter.classList.remove('map--faded');
 
 for (var i = 0; i < adsQuantity; i++) {
-  var xCoordinate = getRandomNumber(mapFilter.clientWidth);
-  var yCoordinate = getRandomIntInclusive(locationYMin, locationYMax);
+  var xCoordinate = getRandomNumber(mapFilter.clientWidth) + 25;
+  var yCoordinate = getRandomIntInclusive(locationYMin, locationYMax) - 165;
   bookingAds[i] = {
     author: {
       avatar: 'img/avatars/user0' + (i + 1) + '.png'
@@ -95,11 +88,9 @@ for (var i = 0; i < adsQuantity; i++) {
   };
 }
 
-for (var i = 0; i < bookingAds.length; i++) {
-  fragment.appendChild(renderBtn(bookingAds[i]));
+for (var j = 0; j < bookingAds.length; j++) {
+  fragment.appendChild(renderBtn(bookingAds[j]));
 }
 
 pins.appendChild(fragment);
-
-console.log(pins);
 
